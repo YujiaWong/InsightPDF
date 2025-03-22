29f3a5bdce1103f78967ef248870f35e25f5fdc4
YujiaWangAaron <wyujia@umich.edu>
2025-03-21 17:25:00 -0700

    test preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div className='flex flex-row justify-center items-center'>
-      123321
+      123321testpreview
       <Button>234</Button>
       <Button>hello world</Button>
 
